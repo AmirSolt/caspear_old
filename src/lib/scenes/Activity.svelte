a66771858381810994fa2762c93cebab07b40bde
AmirSolt <soltamir194@gmail.com>
2023-04-27 04:02:28 -0400

    almost finishing scenes and activities

---
+++ src/lib/scenes/Activity.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let activity;
+
+</script>
+
+
+<div class="container" style="background: url({activity.images[0]});">
+    <h1>{activity.name}</h1>
+    <p>{activity.price} Per Person</p>
+    <p>{activity.description}</p>
+    <a href="{activity.link}">Book Now</a>
+</div>
+    
+
+<style>
+
+.container {
+    position:fixed;
+    padding:0;
+    margin:0;
+
+    top:0;
+    left:0;
+
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    z-index: -1;
+    /* background:rgba(255,255,255,0.5); */
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Activity from "../lib/scenes/Activity.svelte";
+
+
+    const activities = [
+        {
+        name: "Activity #1",
+        price: 100,
+        description: "This is an example activity",
+        link:"https://www.expedia.com/things-to-do/nassau-glass-bottom-boat-banana-boat-and-snorkelling-tour.a4513511.activity-details?endDate=2023-05-11&location=Bahamas&rid=13&selectedId=&sort=RECOMMENDED&startDate=2023-04-27&swp=on",
+        images: ["https://mediaim.expedia.com/localexpert/4513511/3dce46ae-df52-4acf-94e6-4821680d5078.jpg?impolicy=resizecrop&rw=1005&rh=565"]
+        },
+        {
+        name: "Activity #2",
+        price: 200,
+        description: "This is an example activity",
+        link:"https://www.expedia.com/things-to-do/blue-lagoon-beach-day-with-lunch.a301602.activity-details?&rid=13&location=Bahamas&startDate=2023-04-27&endDate=2023-04-27&sort=RECOMMENDED&selectedId=&swp=on",
+        images: ["https://mediaim.expedia.com/localexpert/301602/b0434e79-dcc8-4b81-bf63-1c93a3d2d422.jpg?impolicy=resizecrop&rw=1005&rh=565"]
+        },
+
+    ]
+
+
+    let sceneIndex = 0;
+    $: chosenActivity = activities[sceneIndex];
+
+
+    function previousActivity() {
+        sceneIndex-=1;
+    }
+    function nextActivity() {
+        sceneIndex+=1;
+    }
+
+</script>
+
+<Activity activity={chosenActivity} />
+
+
+<button on:click={previousActivity}>
+    Previous
+</button>
+<button on:click={nextActivity}>
+    Next
+</button>
