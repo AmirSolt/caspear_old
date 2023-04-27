15b166e55cda507548771f3454314d5fee044207
AmirSolt <soltamir194@gmail.com>
2023-04-27 12:02:17 -0400

    finished sticky button

---
+++ src/lib/StickyBook.svelte
@@ -0,0 +1,77 @@
+<script>
+    export let link;
+
+    import sticky from "./sticky.js";
+
+    let stickToTop = true;
+    let isStuck = false;
+
+    function handleStuck(e) {
+        isStuck = e.detail.isStuck;
+    }
+</script>
+
+<section>
+    {#if stickToTop && isStuck}
+        <div class="filler"></div>
+    {/if}
+
+    {#if isStuck}
+        <div
+            class="book-now-container sticky"
+            class:isStuck
+            data-position={stickToTop ? "top" : "bottom"}
+            use:sticky={{ stickToTop }}
+            on:stuck={handleStuck}
+        >
+            <a class="book-now" href={link} target="_blank">Book Now</a>
+        </div>
+    {/if}
+    {#if !isStuck}
+        <div
+            class="book-now-container"
+            class:isStuck
+            data-position={stickToTop ? "top" : "bottom"}
+            use:sticky={{ stickToTop }}
+            on:stuck={handleStuck}
+        >
+            <a class="book-now" href={link} target="_blank">Book Now</a>
+        </div>
+    {/if}
+
+    
+</section>
+
+<style>
+    .book-now-container {
+        display: flex;
+        justify-content: end;
+        width: 100%;
+
+        background-color: var(--primary-transparent);
+
+        overflow: hidden;
+    }
+    .book-now {
+        display: block;
+        background-color: var(--secondary);
+        color: var(--text-secondary);
+        margin: 0.9em 1em;
+        padding: 0.5em 1em;
+        border-radius: var(--radius);
+        border: 1px solid #000;
+        text-decoration: none;
+    }
+
+    .filler{
+        height: 3.5em;
+    }
+
+    .sticky {
+        position: fixed;
+        width: 100%;
+        top: 0;
+        left: 0;
+        transition: all 0.3s;
+    }
+</style>
--- src/lib/scenes/Activity.svelte
+++ src/lib/scenes/Activity.svelte
@@ -1,12 +1,12 @@
 <script>
-
     export let activity;
 
+    import StickyBook from "../StickyBook.svelte";
 
-   
-
-   
 </script>
+
+
+
 
 <div class="activity-container">
     <div class="activity-info">
@@ -14,15 +14,12 @@
         <p>{activity.review_score}/5 ({activity.review_count})</p>
         <p><b>${activity.price}</b> Per Person</p>
         <p>{activity.description}</p>
-        <div class="book-now-container">
-            <a class="book-now" href={activity.link} target="_blank">Book Now</a>
-        </div>
     </div>
+    <StickyBook link={activity.link} />
 
-    <hr>
+    <hr />
 
     <div class="image-container">
-
         {#each activity.images as image}
             <img class="activity-image" src={image} alt={activity.name} />
         {/each}
@@ -30,17 +27,15 @@
 </div>
 
 <style>
-
-    h1{
+    h1 {
         font-size: 2em;
     }
-    p{
+    p {
         font-size: 1.2em;
     }
-    a{
+    a {
         font-size: 1.2em;
     }
- 
 
     .activity-image {
         width: 100%;
@@ -48,57 +43,30 @@
     }
 
     .activity-info {
-
-        position: fixed;
-        top: 0;
-        left: 0;
-        background-color: var(--primary-transparent);
+        /*  */
         padding: 1vmax 2vmax;
         width: 100%;
         height: auto;
         max-height: 16em;
-
     }
 
-    .activity-info > *{
+    .activity-info > * {
         margin: 0.5em 0;
     }
 
-    .image-container{
+    .image-container {
         margin-top: 1em;
     }
 
 
-    .book-now-container{
-        display: flex;
-        justify-content: end;
-        width: 100%;
-    }
-    .book-now{
-        display: block;
-        background-color: var(--secondary);
-        color: var(--text-secondary);
-        margin: 0.9em 0;
-        padding: 0.5em 1em;
-        border-radius: var(--radius);
-        border: 1px solid #000;
-        text-decoration: none;
-    }
 
     /* page size condition */
     @media only screen and (min-width: 600px) {
-        .activity-container {
-            margin-top: 16em;
-        }
-        .activity-info{
+        .activity-info {
             padding: 0 15%;
-
         }
-    
     }
     @media only screen and (max-width: 600px) {
-        .activity-container {
-            margin-top: 16em;
-        }
+        
     }
 </style>
